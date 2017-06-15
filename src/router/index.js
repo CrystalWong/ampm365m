9a252c612e5861fd7a3264de520c9287e9285793
crystal wang <gisele6@126.com>
2017-06-15 17:57:03 +0800

    更改打包配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/Home'
+import Home from '@/components/main/Home'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      components: {
-        default: Home
+  routes: [{
+    path: '/',
+    name: 'Home',
+    components: {
+      default: Home
         // menubar: Menubar
-      }
     }
-  ]
+  }]
 })
