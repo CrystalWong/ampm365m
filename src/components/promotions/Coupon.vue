7951e4cceaff7d99b5a8f3b65decd4bf487893e1
crystal wang <gisele6@126.com>
2017-06-19 15:01:08 +0800

    优惠券界面

--- src/components/promotions/Coupon.vue
+++ src/components/promotions/Coupon.vue
@@ -1,11 +1,14 @@
 <template>
   <div id="coupon" class="set-bg">
     <img src="../../assets/imgs/promotions/coupons/coupon0630/top.png" width="100%">
-    <input type="tel" name="mobile" class="set-bg outer-bg mobile" placeholder="输入手机号">
-    <div class="captcha-wrap">
+    <input type="tel" name="mobile" class="item-wrap set-bg outer-bg mobile" placeholder="输入手机号">
+    <div class="item-wrap captcha-wrap">
       <input type="text" name="captcha" class="set-bg outer-bg captcha fl" placeholder="输入验证码">
       <div class="set-bg outer-bg captcha-get fr">{{ captchaTxt }}</div>
     </div>
+    <div class="item-wrap submit">确定</div>
+    <div class="item-wrap hint"><small>该手机号已经被绑定，请更换手机号后重试</small></div>
+    <img src="../../assets/imgs/promotions/coupons/coupon0630/rules.png" width="80%">
   </div>
 </template>
 
@@ -24,6 +27,7 @@ export default {
 #coupon {
   background-image: url('../../assets/imgs/promotions/coupons/coupon0630/bg.png');
   text-align: center;
+  padding-bottom: 15px;
   .set-bg {
     background-repeat: no-repeat;
     background-size: 100% 100%;
@@ -35,14 +39,15 @@ export default {
     line-height: 45px;
     padding: 10px 20px;
   }
-  .captcha-wrap {
+  .item-wrap {
     width: 80%;
+    margin: 0 auto 5%;
+  }
+  .captcha-wrap {
     overflow: hidden;
-    margin: 0 auto;
   }
   .mobile {
-    width: 80%;
-    margin: 5% auto;
+    margin-top: 5%;
     background-image: url('../../assets/imgs/promotions/coupons/coupon0630/inputfir.png');
   }
   .captcha {
@@ -52,9 +57,19 @@ export default {
   .captcha-get {
     width: 40%;
     padding: 0;
+    color: #f03c31;
     background-image: url('../../assets/imgs/promotions/coupons/coupon0630/captcha.png');
   }
-
-  
+  .submit {
+    background-color: #fe4f42;
+    color: #ffffff;
+    padding: 10px 0;
+    font-size: 0.36rem;
+    border-radius: 30px;
+    margin-bottom: 2%;
+  }
+  .hint {
+    color: #fe4f42;
+  }
 }
 </style>
